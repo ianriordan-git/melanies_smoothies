f0a5d960c4bb9aacfff7626bec5f1c548e88f25e
ianriordan-git <ianriordan@gmail.com>
2026-07-08 12:57:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ if ingredients_list:
         st.success(f" Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
 # New section to display smoothiefroot nutrition info
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
